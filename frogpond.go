2eab076fdc79eaf3d7b285578af970445b252570
Jeremy Price <donomii@gmail.com>
2025-10-19 02:02:27 +0900

    Add backdate param to delete

--- frogpond.go
+++ frogpond.go
@@ -12,11 +12,11 @@ import (
 )
 
 type DataPoint struct {
-    Key     []byte
-    Value   []byte
-    Name    string
-    Updated time.Time
-    Deleted bool
+	Key     []byte
+	Value   []byte
+	Name    string
+	Updated time.Time
+	Deleted bool
 }
 
 // DataPoint represents a single key/value entry.
@@ -26,22 +26,22 @@ type DataPoint struct {
 
 // DeepCopy creates a copy of the DataPoint with Key and Value buffers duplicated
 func (dp DataPoint) DeepCopy() DataPoint {
-    var kCopy, vCopy []byte
-    if dp.Key != nil {
-        kCopy = make([]byte, len(dp.Key))
-        copy(kCopy, dp.Key)
-    }
-    if dp.Value != nil {
-        vCopy = make([]byte, len(dp.Value))
-        copy(vCopy, dp.Value)
-    }
-    return DataPoint{
-        Key:     kCopy,
-        Value:   vCopy,
-        Name:    dp.Name,
-        Updated: dp.Updated,
-        Deleted: dp.Deleted,
-    }
+	var kCopy, vCopy []byte
+	if dp.Key != nil {
+		kCopy = make([]byte, len(dp.Key))
+		copy(kCopy, dp.Key)
+	}
+	if dp.Value != nil {
+		vCopy = make([]byte, len(dp.Value))
+		copy(vCopy, dp.Value)
+	}
+	return DataPoint{
+		Key:     kCopy,
+		Value:   vCopy,
+		Name:    dp.Name,
+		Updated: dp.Updated,
+		Deleted: dp.Deleted,
+	}
 }
 
 type DataPoolList []DataPoint
@@ -51,8 +51,8 @@ func (a DataPoolList) Less(i, j int) bool { return bytes.Compare(a[i].Key, a[j].
 func (a DataPoolList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 type DataPoolMap struct {
-    mu sync.RWMutex
-    m  map[string]DataPoint
+	mu sync.RWMutex
+	m  map[string]DataPoint
 }
 
 func NewDataPoolMap() *DataPoolMap {
@@ -60,19 +60,19 @@ func NewDataPoolMap() *DataPoolMap {
 }
 
 func (dpm *DataPoolMap) Get(key string) (DataPoint, bool) {
-    dpm.mu.RLock()
-    defer dpm.mu.RUnlock()
-    v, ok := dpm.m[key]
-    if !ok {
-        return DataPoint{}, ok
-    }
-    return v.DeepCopy(), ok
+	dpm.mu.RLock()
+	defer dpm.mu.RUnlock()
+	v, ok := dpm.m[key]
+	if !ok {
+		return DataPoint{}, ok
+	}
+	return v.DeepCopy(), ok
 }
 
 func (dpm *DataPoolMap) Set(key string, value DataPoint) {
-    dpm.mu.Lock()
-    defer dpm.mu.Unlock()
-    dpm.m[key] = value.DeepCopy()
+	dpm.mu.Lock()
+	defer dpm.mu.Unlock()
+	dpm.m[key] = value.DeepCopy()
 }
 
 func (dpm *DataPoolMap) Delete(key string) {
@@ -82,41 +82,41 @@ func (dpm *DataPoolMap) Delete(key string) {
 }
 
 func (dpm *DataPoolMap) ForEach(f func(string, DataPoint)) {
-    dpm.mu.RLock()
-    defer dpm.mu.RUnlock()
-    for k, v := range dpm.m {
-        f(k, v.DeepCopy())
-    }
+	dpm.mu.RLock()
+	defer dpm.mu.RUnlock()
+	for k, v := range dpm.m {
+		f(k, v.DeepCopy())
+	}
 }
 
 func (dpm *DataPoolMap) ToList() DataPoolList {
-    dpm.mu.RLock()
-    defer dpm.mu.RUnlock()
-    out := DataPoolList{}
-    for _, v := range dpm.m {
-        out = append(out, v.DeepCopy())
-    }
-    sort.Sort(out)
-    return out
+	dpm.mu.RLock()
+	defer dpm.mu.RUnlock()
+	out := DataPoolList{}
+	for _, v := range dpm.m {
+		out = append(out, v.DeepCopy())
+	}
+	sort.Sort(out)
+	return out
 }
 
 func (dpm *DataPoolMap) FromList(dl DataPoolList) {
-    dpm.mu.Lock()
-    defer dpm.mu.Unlock()
-    for _, v := range dl {
-        dpm.m[string(v.Key)] = v.DeepCopy()
-    }
+	dpm.mu.Lock()
+	defer dpm.mu.Unlock()
+	for _, v := range dl {
+		dpm.m[string(v.Key)] = v.DeepCopy()
+	}
 }
 
 type Node struct {
-    DataPool       *DataPoolMap
-    Debug          bool
-    EnablePullData bool // If false, disables active pulling of data from peers (saves memory/processing)
+	DataPool       *DataPoolMap
+	Debug          bool
+	EnablePullData bool // If false, disables active pulling of data from peers (saves memory/processing)
 }
 
 // Create a new frogpond node
 func NewNode() *Node {
-    return &Node{DataPool: NewDataPoolMap()}
+	return &Node{DataPool: NewDataPoolMap()}
 }
 
 // Convert a map of data points to a list of data points
@@ -164,15 +164,15 @@ func (n *Node) debugf(f string, v ...interface{}) {
 
 // Dump the entire data pool as a json array
 func (n *Node) JsonDump() []byte {
-    out, err := json.Marshal(n.DataPool.ToList())
-    if err != nil {
-        log.Println("Failed to marshal data pool", err)
-        return nil
-    }
-    // Return a copy to avoid exposing internal buffer
-    copied := make([]byte, len(out))
-    copy(copied, out)
-    return copied
+	out, err := json.Marshal(n.DataPool.ToList())
+	if err != nil {
+		log.Println("Failed to marshal data pool", err)
+		return nil
+	}
+	// Return a copy to avoid exposing internal buffer
+	copied := make([]byte, len(out))
+	copy(copied, out)
+	return copied
 }
 
 // Set a single data point
@@ -212,42 +212,50 @@ func (n *Node) GetDataPointWithPrefix(prefix, key string) DataPoint {
 	return n.GetDataPoint(keyStr)
 }
 
-func (n *Node) DeleteDataPoint(keyStr string) []DataPoint {
-	dp := DataPoint{Key: []byte(keyStr), Deleted: true, Updated: time.Now()}
+// Delete a data point, and backdate it by timeDuration.  Backdating allows for a grace period,
+// to allow replication to propogate a recent update.
+//
+//	e.g. It is naturally hard to detect when a node drops out of the network.  So the nodes constantly
+//
+// try to delete each other, backdated by 30 minutes.  If a node has published an update in that 30
+// minutes, the delete will be ignored.  This is a natural way to handle partitions.  The partitioned
+// nodes keep updating, and when the network rejoins, the deletes will not remove valid nodes
+func (n *Node) DeleteDataPoint(keyStr string, backdateDuration time.Duration) []DataPoint {
+	dp := DataPoint{Key: []byte(keyStr), Deleted: true, Updated: time.Now().Add(-backdateDuration)}
 	n.DataPool.Set(keyStr, dp)
 	return []DataPoint{dp}
 }
 
-func (n *Node) DeleteDataPointWithPrefix(prefix, key string) []DataPoint {
+func (n *Node) DeleteDataPointWithPrefix(prefix, key string, backdateDuration time.Duration) []DataPoint {
 	keyStr := fmt.Sprintf("%v%v", prefix, key)
-	dp := DataPoint{Key: []byte(keyStr), Deleted: true, Updated: time.Now()}
+	dp := DataPoint{Key: []byte(keyStr), Deleted: true, Updated: time.Now().Add(-backdateDuration)}
 	n.DataPool.Set(keyStr, dp)
 	return []DataPoint{dp}
 }
 
 func (n *Node) DeleteAllMatchingPrefix(keyStr string) []DataPoint {
-    out := []DataPoint{}
-    n.DataPool.ForEach(func(k string, v DataPoint) {
-        if strings.HasPrefix(k, keyStr) {
-            v.Deleted = true
-            v.Updated = time.Now()
-            n.DataPool.Set(k, v)
-            // Append a deep copy to avoid sharing buffers
-            out = append(out, v.DeepCopy())
-        }
-    })
-    return out
+	out := []DataPoint{}
+	n.DataPool.ForEach(func(k string, v DataPoint) {
+		if strings.HasPrefix(k, keyStr) {
+			v.Deleted = true
+			v.Updated = time.Now()
+			n.DataPool.Set(k, v)
+			// Append a deep copy to avoid sharing buffers
+			out = append(out, v.DeepCopy())
+		}
+	})
+	return out
 }
 
 func (n *Node) GetAllMatchingPrefix(keyStr string) []DataPoint {
-    out := []DataPoint{}
-    n.DataPool.ForEach(func(k string, v DataPoint) {
-        if strings.HasPrefix(k, keyStr) {
-            // Append a deep copy to avoid sharing buffers
-            out = append(out, v.DeepCopy())
-        }
-    })
-    return out
+	out := []DataPoint{}
+	n.DataPool.ForEach(func(k string, v DataPoint) {
+		if strings.HasPrefix(k, keyStr) {
+			// Append a deep copy to avoid sharing buffers
+			out = append(out, v.DeepCopy())
+		}
+	})
+	return out
 }
 
 // Add or update a list of data points to the data pool
@@ -265,13 +273,13 @@ func (n *Node) AppendDataPoint(dataPoint DataPoint) []DataPoint {
 }
 
 type Config struct {
-    HttpPort           uint
-    StartPagePort      uint
-    Name               string
-    MaxUploadSize      uint
-    Networks           []string
-    KnownPeers         []string
-    ArpCheckInterval   int
-    PeerUpdateInterval int
-    Debug              bool
+	HttpPort           uint
+	StartPagePort      uint
+	Name               string
+	MaxUploadSize      uint
+	Networks           []string
+	KnownPeers         []string
+	ArpCheckInterval   int
+	PeerUpdateInterval int
+	Debug              bool
 }
